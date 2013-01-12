8a99469ad7582b4ee365aaeb43c8e64ac9e56df5
pascal <pascal@graal-laptop.(none)>
2013-01-12 15:46:54 -0500

    ...

--- user-interface/blitz/blitz/views.py
+++ user-interface/blitz/blitz/views.py
@@ -11,57 +11,50 @@ _images = _data+'/BlitzDataWebService/images/'
 #_tmp = '{"facets": {"origin": {"Georgia": 1,"White Plains": 1},"type": {"albums": 32,"artists": 19},"genres": {"Alternative rock": 2,"Pop rock": 1,"Indie rock": 1}},"results": [{"id": "011_x9"} ]}'
 _tmp = '{"facets":{"FACET_0_NAME": {"KEY_0_NAME":10,"KEY_1_NAME":10,"KEY_2_NAME":10,"KEY_3_NAME":10},"FACET_1_NAME": {"KEY_0_NAME":10,"KEY_1_NAME":10,"KEY_2_NAME":10,"KEY_3_NAME":10},"FACET_2_NAME": {"KEY_0_NAME":10,"KEY_1_NAME":10,"KEY_2_NAME":10,"KEY_3_NAME":10}}, "results": [ {"id":0,"text":"Document description"},{"id":1,"text":"Document description"},{"id":2,"text":"Document description"},{"id":3,"text":"Document description"},{"id":4,"text":"Document description"},{"id":5,"text":"Document description"},{"id":6,"text":"Document description"},{"id":7,"text":"Document description"},{"id":8,"text":"Document description"},{"id":9,"text":"Document description"},{"id":10,"text":"Document description"},{"id":11,"text":"Document description"},{"id":12,"text":"Document description"},{"id":13,"text":"Document description"},{"id":14,"text":"Document description"}]} '
 
+_SESSIONS = dict()
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     
     return {'project': 'blitz'}
 
-@view_config(route_name='search', renderer='templates/pascal2.pt')
-def main_search(request):
-    s = request.GET['search']
-    r = requests.get(_endpoint, params={'q':s})
-    #elem = json.loads(r.content)
-    D = json.loads(_tmp)
-    
-    
+def gen_facets(D):
     FACES = ''
-    if D.has_key('facets'):
-        i = 0
-        for section, fval in D['facets'].iteritems():
+    i = 0
+    for section, fval in D.iteritems():
             
-            #rint '*********', section , '*********'
-            i += 1
-            FACES += """<div class="accordion"> <div class="accordion-heading">
+        #rint '*********', section , '*********'
+        i += 1
+        FACES += """<div class="accordion"> <div class="accordion-heading">
                               <a class="accordion-toggle" data-parent="#accordion""" + str(i) + """" data-toggle="collapse" href="#panel""" + str(i) + """"><i class="icon-chevron-right"></i>"""
-            FACES += section
-            FACES += """</a>
-                          </div>
-                          <div class="accordion-body collapse" id="panel""" + str(i) + """">
-                              <div class="accordion-inner">"""
-            FACES += '<ul>'
+        FACES += section
+        FACES += """</a>
+                    </div>
+                    <div class="accordion-body collapse" id="panel""" + str(i) + """">
+                            <div class="accordion-inner">"""
+        FACES += '<ul>'
             
-            for nom,  nb in fval.iteritems():
-                FACES += '<li> ' + nom + ' (' + str(nb) + ')  </li>'
-                #print nom
-                #print nb
+        for nom,  nb in fval.iteritems():
+            FACES += '<li> ' + nom + ' (' + str(nb) + ')  </li>'
+            #print nom
+            #print nb
                 
-            FACES += '</ul></div></div></div>'
-            
+        FACES += '</ul></div></div></div>'
+    return FACES
+ 
+def gen_results(L):
     RESULTS = '<ul class="thumbnails">'
-    if D.has_key('results'):
-        for item in D['results']:
-            name = '...'
-            text = '...'
-            if item.has_key('id') : 
-                id = item['id']
-                name = str(id)
-                img_url = _images + str(id)
-            if item.has_key('text') : name = str(item['text'])
+
+    for item in L:
+        name = '...'
+        text = '...'
+        if item.has_key('id') : 
+            id = item['id']
+            name = str(id)
+            img_url = _images + str(id)
+        if item.has_key('text') : name = str(item['text'])
             
-            
-            
-            RESULTS += """                      <li class="span9">
+        RESULTS += """                      <li class="span9">
                           <div class="row">
                               <div class="span3">
                                   <figure >
@@ -81,9 +74,40 @@ def main_search(request):
 
                           </div>
 
-                      </li>""" %  {'name':name, 'text':text, 'img_url':img_url}           
+            </li>""" %  {'name':name, 'text':text, 'img_url':img_url}           
                       
     RESULTS +=  '</ul>'
-        
+    return RESULTS
+                    
+
+@view_config(route_name='search', renderer='templates/pascal2.pt')
+def main_search(request):
+    s = request.GET['search']
+    r = requests.get(_endpoint, params={'q':s})
+    #elem = json.loads(r.content)
+    D = json.loads(_tmp)
+    
+    if D.has_key('facets'):
+        FACES = gen_facets(D['facets'])
+
+    if D.has_key('results'):
+        RESULTS = gen_results(D['results'])
+
             
     return {'faces':FACES, 'results':RESULTS}
+
+@view_config(route_name='rafine', renderer='string')
+def rafine_search(request):
+    s = request.GET['search']
+    r = requests.get(_endpoint, params={'q':s})
+    #elem = json.loads(r.content)
+    D = json.loads(_tmp)
+    
+    if D.has_key('results'):
+        RESULTS = gen_results(D['results'])
+        
+    return RESULTS
+        
+    
+
+    
