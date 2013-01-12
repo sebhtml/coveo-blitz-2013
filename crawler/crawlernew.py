bd4f2a0e2c4de02223e19beb2b062184fd975279
Alexandre Drouin <alexandre.drouin1@gmail.com>
2013-01-12 13:30:30 -0500

    error handling

--- crawler/crawlernew.py
+++ crawler/crawlernew.py
@@ -5,7 +5,7 @@ from multiprocessing import Process, Pool
 from time import time
 
 data_service = 'http://ec2-204-236-255-208.compute-1.amazonaws.com:8080/BlitzDataWebService/'
-
+max_retries = 10
 
 def start_run(run_id):
     print requests.get(data_service + 'evaluationRun/start', params={'runId':run_id}).url
@@ -14,22 +14,30 @@ def stop_run():
     requests.get(data_service + 'evaluationRun/stop')
 
 def get_artist(id):
-    #print 'Hello worker ARTIST'
-    request = requests.get(data_service + 'artists/' + str(id))
+    for i in xrange(max_retries):
+        try:
+            request = requests.get(data_service + 'artists/' + str(id))
+            break;
+        except:
+            pass
     data = dict(json.loads(request.content))
     data['type'] = u'artist'
     data = trim_ends(data)
     index_data(data)
-    print data['name']
+    #print data['name']
 
 def get_album(id):
-    #print 'Hello worker ALBUM'
-    request = requests.get(data_service + 'albums/' + str(id))
+    for i in xrange(max_retries):
+        try:
+            request = requests.get(data_service + 'albums/' + str(id))
+            break;
+        except:
+            pass
     data = dict(json.loads(request.content))
     data['type'] = u'album'
     data = trim_ends(data)
     index_data(data)
-    print data['name']
+    #print data['name']
 
 def crawl_artists():
     page_size = 100
@@ -53,12 +61,8 @@ def crawl_artists():
 
         for artist in artists:
             # Get artists data async
-            workers.apply_async(get_artist, args=(artist['id'],), callback=hello)
-            #count +=1
-            #print 'worker submit'
+            workers.apply_async(get_artist, args=(artist['id'],))
 
-def hello(test):
-    print 'Hello!', test
 
 def crawl_albums():
     page_size = 100
@@ -81,8 +85,7 @@ def crawl_albums():
 
         for album in albums:
             # Get artists data async
-            workers.apply_async(get_album, args=(album['id'],), callback=hello)
-            #count +=1
+            workers.apply_async(get_album, args=(album['id'],))
 
 def callback_artist(request):
     pass
@@ -96,7 +99,6 @@ def callback_album(request):
 
 def index_data(data):
     requests.post('http://ec2-50-17-15-66.compute-1.amazonaws.com:11111/Korafle.cgi/push', data=data)
-    #count += 1
 
 def trim_ends(object):
     if unicode == type(object):
@@ -116,28 +118,15 @@ def trim_ends(object):
 if __name__ == "__main__":
     t = time()
     start_run('Run1test')
-    #count = 0
+
     artists_crawler = Process(target=crawl_artists)
     albums_crawler = Process(target=crawl_albums)
-    workers = Pool(50)
+    workers = Pool(200)
 
     crawl_artists()
-    #crawl_artists()
     crawl_albums()
-    #crawl_albums()
-
-
-
-    #albums_crawler.start()
-    #artists_crawler.start()
-
-    #albums_crawler.join()
-    #artists_crawler.join()
 
     workers.close()
     workers.join()
 
     print "Took: ", time()-t
-
-    from time import sleep
-    #sleep(10)
